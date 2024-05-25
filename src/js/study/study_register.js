92c970c9eab4e9f58a4bb509767f5845236401a4
iieunji023 <iieunji023@gmail.com>
2024-05-25 21:28:10 +0900

    study 등록 html 진행중

--- src/js/study/study_register.js
+++ src/js/study/study_register.js
@@ -1,7 +1,7 @@
 // study_register.html 내용을 로드해서 삽입하는 함수
 function loadStudyRegister() {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', '/src/html/study/study_register.html', true); // 경로는 실제 study_register.html의 경로로 변경해야 함
+  xhr.open('GET', '../../html/study/study_register.html', true); // 경로는 실제 study_register.html의 경로로 변경해야 함
   xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
           document.querySelector('.contents-box').innerHTML = xhr.responseText;
