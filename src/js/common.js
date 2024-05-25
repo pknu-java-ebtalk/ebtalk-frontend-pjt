fa5ed0bf9b196ba75e7fd30bd1bb6c988a30955a
iieunji023 <iieunji023@gmail.com>
2024-05-25 21:32:37 +0900

    html 연결을 위한 common.js 생성

--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,8 @@
+// main.js
 // study_register.html 내용을 로드해서 삽입하는 함수
 function loadHtml() {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', '../../html/study/study_register.html', true); // 경로는 실제 study_register.html의 경로로 변경해야 함
+  xhr.open('GET', 'html파일 상대경로 입력', true); // 경로는 실제 study_register.html의 경로로 변경해야 함
   xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
           document.querySelector('.contents-box').innerHTML = xhr.responseText;
@@ -10,5 +11,4 @@ function loadHtml() {
   xhr.send();
 }
 
-// 페이지 로드 시 스터디 등록 폼을 불러옴
 window.addEventListener('load', loadHtml);
