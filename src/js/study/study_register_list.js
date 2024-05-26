51d38b7d0d332fe02539fd08b10900e30490d203
iieunji023 <iieunji023@gmail.com>
2024-05-26 17:10:08 +0900

    스터디 모집 리스트 html/css 완료

---
+++ src/js/study/study_register_list.js
@@ -0,0 +1,13 @@
+// study_register.html 내용을 로드해서 삽입하는 함수
+function loadHtml() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', '../../html/study/study_register_list.html', true); // 경로는 실제 study_register.html의 경로로 변경해야 함
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          document.querySelector('.contents-box').innerHTML = xhr.responseText;
+      }
+  };
+  xhr.send();
+}
+
+window.addEventListener('load', loadHtml);
