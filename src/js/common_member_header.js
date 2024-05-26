f21bbd7a13e3e07d097533f734f94866334e6929
hyanyul <tjdwl0808@gmail.com>
2024-05-26 17:23:09 +0900

    기본틀 변경 최종, 로그인 페이지 완성

---
+++ src/js/common_member_header.js
@@ -0,0 +1,14 @@
+// main.js
+// study_register.html 내용을 로드해서 삽입하는 함수
+function loadHtml() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', '../common/common_member_header.html', true); // 경로는 실제 study_register.html의 경로로 변경해야 함
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          document.querySelector('header').innerHTML = xhr.responseText;
+      }
+  };
+  xhr.send();
+}
+
+window.addEventListener('load', loadHtml);
