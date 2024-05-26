0e16acd0e4f29eab43727e54f0a62d6c63db3f3d
hyanyul <tjdwl0808@gmail.com>
2024-05-27 04:06:38 +0900

    마이페이지-회원정보 진행중

--- src/js/member/user_mypage_aside.js
+++ src/js/member/user_mypage_aside.js
@@ -2,7 +2,7 @@
 // study_register.html 내용을 로드해서 삽입하는 함수
 function loadHtml() {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', '../../html/member/user_mypage_info_aside.html', true); // 본인이 side에 '넣을' 코드 적힌 파일 상대경로 작성
+  xhr.open('GET', '../../html/member/user_mypage_aside.html', true); // 본인이 side에 '넣을' 코드 적힌 파일 상대경로 작성
   xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
           document.querySelector('#aside-contents-box').innerHTML = xhr.responseText;
